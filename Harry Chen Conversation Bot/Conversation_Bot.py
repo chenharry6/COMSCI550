30e8bcf41616153f5654791a2ac9ef33e277336f
Harry Chen <hchen22@choate.edu>
2019-09-09 22:01:37 -0400

    This is a Conversation Bot for CS550

---
+++ Harry Chen Conversation Bot/Conversation_Bot.py
@@ -0,0 +1,32 @@
+#Harry 
+#09/06/19
+#ConversationBOT
+#None 
+#OMH 
+
+value=input("\nHey there, what's your name?\n\n") 
+
+print("\nWhat's up "+value + ". How's your day been going?\n") #CPU response to user input 
+value=input("")
+
+print("\nI'm "+ value +" as well\n") #CPU response to user input. 
+ 
+print("What do you like to do in your free time?\n") 
+
+input("") #user input 
+
+print("\nThat's cool, I like to talk to my friend Alexa. "+ 
+	"\nWhat are your friends' names?\n")
+
+value=input("") #user input 
+
+print("\nI don't know. " +value) #CPU response to user input 
+
+print("\nAlright,it's been nice talking to you."+
+	"I gotta go now,see you tomorrow!\n")
+	
+input("") 
+
+
+
+
